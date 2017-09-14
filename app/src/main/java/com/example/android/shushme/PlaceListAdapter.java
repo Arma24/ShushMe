3c988ba98180ba16bd81d606fbd53c3c1ce49a49
Arma24 <arma0857@gmail.com>
2017-09-15 06:56:14 +0700

    - Geofencing

--- app/src/main/java/com/example/android/shushme/PlaceListAdapter.java
+++ app/src/main/java/com/example/android/shushme/PlaceListAdapter.java
@@ -64,15 +64,12 @@ public class PlaceListAdapter extends RecyclerView.Adapter<PlaceListAdapter.Plac
      */
     @Override
     public void onBindViewHolder(PlaceViewHolder holder, int position) {
-        // TODO (6) Implement onBindViewHolder to set the view holder's Name and Address text fields
-        // from the Place object at the specified position in mPlaces
         String placeName = mPlaces.get(position).getName().toString();
         String placeAddress = mPlaces.get(position).getAddress().toString();
         holder.nameTextView.setText(placeName);
         holder.addressTextView.setText(placeAddress);
     }
 
-    //TODO (7) Implement a public method swapPlaces that replaces the current mPlaces PlaceBuffer with a new one
     public void swapPlaces(PlaceBuffer newPlaces) {
         mPlaces = newPlaces;
         if (mPlaces != null) {
@@ -87,7 +84,6 @@ public class PlaceListAdapter extends RecyclerView.Adapter<PlaceListAdapter.Plac
      */
     @Override
     public int getItemCount() {
-        // TODO (5) Update getItemCount to return mPlaces's item count
         if (mPlaces == null) return 0;
         return mPlaces.getCount();
     }
